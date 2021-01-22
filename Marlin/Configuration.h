ed4058c4b1bb4afe736a4be7c746254feb895705
Olivier de Broqueville <bergerac01@xerus.be>
2021-01-22 18:43:45 +0100

    BQ-BF240

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "OdB BQ BLT BF239 UBL  "
+#define CUSTOM_MACHINE_NAME "OdB BQ BLT BF240 UBL  "
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
